712a1fb29fa0a56904fc7d2f1de805be10c0388b
ericyork <eyork@clarkson.edu>
2023-03-07 13:10:13 -0500

    initial import

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+  import '../global.css';
+</script>
+
+<nav class="main-nav">
+  <a href="/">Home</a>
+  <a href="/posts">Posts</a>
+  <a href="/favs">Favs</a>
+  <a href="/info">Info</a>
+  <a href="/about">About</a>
+  <a href="/donate">Donate</a>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <a href="/">&copy;2023 Eric York</a>
+</footer>
+
+<style>
+  nav, footer {
+    display: flex;
+    justify-content: space-evenly;
+    padding: .5em;
+    background-color:honeydew;
+    color: seagreen;
+  }
+  nav {
+    font-weight: 900;
+    text-transform: uppercase;
+  }
+  main {
+    grid-row: 2;
+    padding: 0 2rem;
+    background-color: rgba(255,255,255,.05);
+  }
+  footer {
+    font-size: 75%;
+    grid-row: 3;
+    position: sticky;
+    bottom: 0;
+  }
+</style>
